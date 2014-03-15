8addc02d3cf7c221f65cb14c4201f91e21991d9d
Daniel Hug <danielthehug@gmail.com>
2014-03-14 22:53:14 -0700

    Fix JS for last commit & add a nested demo

--- panels.js
+++ panels.js
@@ -1,36 +1,32 @@
 // toggleable tabbed panels
-(function($) {
-	$('.tabbed-panels').forEach(function(parent) {
-		var tabsParent = $('tabs', parent)[0];
-		var tabs = $('tab', tabsParent || parent);
-		var panels = $('panel', parent);
-		var closeable = parent.classList.contains('closeable');
+[].forEach.call(document.getElementsByClassName('tabbed-panels'), function(parent) {
+	var tabs = this('tab', this('tabs', parent)[0] || parent);
+	var panels = this('panel', parent);
+	var closeable = parent.classList.contains('closeable');
 
-		tabs.forEach(function(tab, i) {
-			tab.addEventListener('click', function() {
-				tabs.forEach(function(tab) {
-					if (tab != this) tab.classList.remove('active');
-				}, this);
+	tabs.forEach(function(tab, i) {
+		tab.addEventListener('click', function() {
+			tabs.forEach(function(tab) {
+				if (tab != this) tab.classList.remove('active');
+			}, this);
 
-				if (closeable) {
-					var isActive = this.classList.toggle('active');
-					parent.classList[isActive ? 'add' : 'remove']('active');
-				} else {
-					this.classList.add('active');
-					parent.classList.add('active');
-				}
+			if (closeable) {
+				var isActive = this.classList.toggle('active');
+				parent.classList[isActive ? 'add' : 'remove']('active');
+			} else {
+				this.classList.add('active');
+				parent.classList.add('active');
+			}
 
-				panels.forEach(function(panel, j) {
-					panel.classList[j === i ? (closeable ? 'toggle' : 'add') : 'remove']('active');
-				});
+			panels.forEach(function(panel, j) {
+				panel.classList[j === i ? (closeable ? 'toggle' : 'add') : 'remove']('active');
 			});
 		});
 	});
 
 // Get children with class:
-})(function(className, scopeEl) {
-	var elements = (scopeEl || document).getElementsByClassName(className);
-	return scopeEl ? [].filter.call(elements, function(child) {
+}, function(className, scopeEl) {
+	return [].filter.call(scopeEl.getElementsByClassName(className), function(child) {
 		return child.parentNode === scopeEl;
-	}) : elements;
+	});
 });
